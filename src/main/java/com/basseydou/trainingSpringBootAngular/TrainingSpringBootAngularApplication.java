1ad32dfe9155c5774e2940156dd24275734c1e4d
Basseydou Ouattara <bassouat8@gmail.com>
2022-09-20 18:05:46 +0200

    Creating application wit spring initializer

---
+++ src/main/java/com/basseydou/trainingSpringBootAngular/TrainingSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package com.basseydou.trainingSpringBootAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringBootAngularApplication.class, args);
+	}
+
+}
